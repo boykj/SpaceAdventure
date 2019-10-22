be4de89199287242f17e121406ae5e678564c77c
boykj <jboyk918@gmail.com>
2019-10-21 21:44:47 -0700

    Got yes or no basic implementation working with print functions for yes or no. Also working on hard coding a dictionary for the planet data as opposed to file input/reading.

--- main.go
+++ main.go
@@ -17,8 +17,13 @@ func main() {
 	fmt.Println("Nice to meet you,", input)
 	fmt.Println("My name is drangus, I'm an old friend of Kirk. Lets go on an adventure!")
 	
+	p := make(map[string][]string)
+	
+	p["A"] = []string{"1", "2"}
+	p["B"] = []string{"3", "4"}
+	fmt.Println(p["A"])
+	
 	yesNo()
-
 }
 
 func getName() {
@@ -49,8 +54,10 @@ func yesNo() bool {
 		response = strings.ToLower(strings.TrimSpace(response))
 
 		if response == "y" || response == "yes" {
+			fmt.Println("Okay, select a planet")
 			return true
 		} else if response == "n" || response == "no" {
+			fmt.Println("Fair enough, I'll choose for you")
 			return false
 		}
 	}
