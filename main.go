cf5e3123a5f7b8452bbb0a2c28f861d00cb2826c
boykj <jboyk918@gmail.com>
2019-10-22 10:01:58 -0700

    Attempted imlementation of planet selection function given the user submits yes. Current work in progress and pretty ugle, but I'm learning decent functionality with the language just running through code

--- main.go
+++ main.go
@@ -11,28 +11,26 @@ import (
 func main() {
 
 	//Display the welcome message
-	
 	welcome()
 	
 	//Early dictionary creation for hard coding the planetary values
 	//Creates a map of strings
-	
 	p := make(map[string][]string)
-	p["Mc"] = []string{"Name: ", "Mercury", " || Description: ", "A very hot planet, closest to the sun"}
-	p["Vn"] = []string{"Venus", "It's very cloudy here!"}
-	p["Ea"] = []string{"Earth", "There is something very familiar about this planet"}
-	p["Mr"] = []string{"Mars", "Known as the red planet."}
-	p["Jp"] = []string{"Jupiter", "A gas giant, with a noticeable red spot."}
-	p["Sa"] = []string{"Saturn", "This planet has beautiful rings around it."}
-	p["Ur"] = []string{"Uranus", "Strangely, this planet rotates around on its side."}
-	p["Np"] = []string{"Neptune", "A very cold planet, furthest from the sun."}
-	p["Pl"] = []string{"Pluto", "I don't care what they say - it's a planet."}
+	p["Mercury"] = []string{"Name: ", "Mercury", " || Description: ", "A very hot planet, closest to the sun"}
+	p["Venus"] = []string{"Venus", "It's very cloudy here!"}
+	p["Earth"] = []string{"Earth", "There is something very familiar about this planet"}
+	p["Mars"] = []string{"Mars", "Known as the red planet."}
+	p["Jupiter"] = []string{"Jupiter", "A gas giant, with a noticeable red spot."}
+	p["Saturn"] = []string{"Saturn", "This planet has beautiful rings around it."}
+	p["Uranus"] = []string{"Uranus", "Strangely, this planet rotates around on its side."}
+	p["Neptune"] = []string{"Neptune", "A very cold planet, furthest from the sun."}
+	p["Pluto"] = []string{"Pluto", "I don't care what they say - it's a planet."}
 	
 	//fmt.Println(p["Mc"])
 	//fmt.Println(p["Vn"])
 	
 	for k, v := range p {
-		fmt.Println("k: ", k, "v: ", v)
+		fmt.Println("Planet: ", k, "Description: ", v)
 	}
 	
 	//Prompt user to enter their name and echo
@@ -62,6 +60,21 @@ func welcome() {
 	fmt.Println("There are 9 planets to explore")
 }
 
+func pSelect(map) {
+		
+		fmt.Println("Please select a planet from our solar system")
+		fmt.Print("Planet name: ")
+		var pInput string
+		fmt.Scanln(&pInput)
+
+		for k, v := range map {
+			if pInput == k {
+			fmt.Println(k, v)
+			
+			}
+		}
+}
+
 func yesNo() bool {
 	reader := bufio.NewReader(os.Stdin)
 
@@ -76,13 +89,28 @@ func yesNo() bool {
 		response = strings.ToLower(strings.TrimSpace(response))
 
 		if response == "y" || response == "yes" {
-			fmt.Println("Okay, select a planet")
+		
+			//If yes, display prompt to user to enter a planet name and print corresponding sentences
+			
+			fmt.Println("Okay, select a planet from our solar system")
+			fmt.Print("Planet name: ")
+			var input string
+			fmt.Scanln(&input)
+			pSelect()
 			return true
+			
 		} else if response == "n" || response == "no" {
+			
+			//If no, return false and randomly choose a planet
+			
 			fmt.Println("Fair enough, I'll choose for you")
 			return false
 		} else {
 			fmt.Println("Please enter a valid answer Y/N, or Yes / No")
 		}
 	}
+
 }
+
+
+
