12ae99f5e8e0a0bdb4a9082372a146fa38a59820
boykj <jboyk918@gmail.com>
2019-10-21 19:05:25 -0700

    Have valid user input and have been sourcing a yes/no function with refactoring to make it work for this assignment

--- main.go
+++ main.go
@@ -5,27 +5,53 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"log"
 )
 
 func main() {
 
 	welcome()
-	getName()
+	fmt.Print("Enter your name: ")
+	var input string
+	fmt.Scanln(&input)
+	fmt.Println("Nice to meet you,", input)
+	fmt.Println("My name is drangus, I'm an old friend of Kirk. Lets go on an adventure!")
+	
+	yesNo()
 
 }
 
 func getName() {
 	for {
-		reader := bufio.NewReader(os.Stdin)
-		var name string
-		fmt.Println("What is your name?")
-		name, _ := reader.ReadString('\n')
-		name = strings.Replace(name, "\r\n", "", -1)
-		fmt.Println("Your name is ", name)
+		fmt.Println("What's your name?")
+		var input string
+		fmt.Scanln(&input)
+		fmt.Println(input)
 	}
 }
 
 func welcome() {
 	fmt.Println("Welcome to the Solar System!")
 	fmt.Println("There are 9 planets to explore")
+}
+
+func yesNo() bool {
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Printf("[y/n]:")
+
+		response, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		response = strings.ToLower(strings.TrimSpace(response))
+
+		if response == "y" || response == "yes" {
+			return true
+		} else if response == "n" || response == "no" {
+			return false
+		}
+	}
 }
